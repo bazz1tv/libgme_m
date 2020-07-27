bb6866633ea72d94140323afa09ca0285c3d1d6e
Michael Bazzinotti <bazz@bazz1.com>
2020-07-27 03:11:48 -0400

    remove cpp11 stuff

--- gme_m/Spc_Report.cpp
+++ gme_m/Spc_Report.cpp
@@ -5,3 +5,11 @@ int Spc_Report::last_pc = -1;
 unsigned char Spc_Report::used[0x10006];
 unsigned char Spc_Report::used2[0x101];
 Spc_Report::Src Spc_Report::src[MAX_SRCN_ENTRIES];
+
+Spc_Report::Src::Src() :
+  brr_start(0xffff),
+  brr_end(0xffff),
+  brr_loop_start(0xffff),
+  brr_loop_end(0xffff),
+  brr_loop_active(false)
+{}
--- gme_m/Spc_Report.h
+++ gme_m/Spc_Report.h
@@ -5,24 +5,24 @@
 
 //:< Why are these #defines and is there a cleaner way?
 #define spc_report_mem_read(addr) do {\
-  Spc_Report::obj->report(Spc_Report::Type::Read, addr, 0);\
+  Spc_Report::obj->report(Spc_Report::Read, addr, 0);\
 } while(0)
 
 #define spc_report_mem_echo(addr) do {\
-  Spc_Report::obj->report(Spc_Report::Type::Echo, addr, 0);\
+  Spc_Report::obj->report(Spc_Report::Echo, addr, 0);\
 } while(0)
 
 #define spc_report_mem_execute(addr, opcode) do {\
   Spc_Report::obj->report(\
-    Spc_Report::Type::Execute, addr, opcode);\
+    Spc_Report::Execute, addr, opcode);\
 } while(0)
 
 #define spc_report_mem_write(addr) do {\
-  Spc_Report::obj->report(Spc_Report::Type::Write, addr, 0);\
+  Spc_Report::obj->report(Spc_Report::Write, addr, 0);\
 } while(0)
 
 #define spc_report_tracker(cmd, arg) do {\
-	Spc_Report::obj->report(Spc_Report::Type::TrackerCmd, cmd, arg);\
+	Spc_Report::obj->report(Spc_Report::TrackerCmd, cmd, arg);\
 } while(0)
 
 class Spc_Report
@@ -48,12 +48,13 @@ public:
   //:< what is this for? why is it called Src?
   struct Src
   {
+    Src();
     uint16_t dir_addr; //unused for now
-    uint16_t brr_start=0xffff;
-    uint16_t brr_end=0xffff;
-    uint16_t brr_loop_start=0xffff;
-    uint16_t brr_loop_end=0xffff;
-    bool     brr_loop_active=false;
+    uint16_t brr_start;
+    uint16_t brr_end;
+    uint16_t brr_loop_start;
+    uint16_t brr_loop_end;
+    bool     brr_loop_active;
   };
   static Src src[MAX_SRCN_ENTRIES];
 
